cee2ceaa82dc1dba08f4059f91cb0a91df0b5ccc
Daniel Esteban <danielgalvis.3107@gmail.com>
2025-07-28 15:27:44 -0500

    correccion del error de la importacion para redirigir la vista inventario

--- tiamariweb/urls.py
+++ tiamariweb/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
